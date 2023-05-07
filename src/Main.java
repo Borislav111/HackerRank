e77912e2fbafe9a2c9bdbf63c2ac9c15929c60f0
Borislav111 <btraychev@gmail.com>
2023-05-07 15:15:33 +0300

    Second commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main extends EasyEx {
+    public static void main(String[] args) {
+//        System.out.println(EasyEx.convertToString());
+//        System.out.println();
+//
+//        currencyFormatter();
+        // workWithStr();
+        String chooseStartAndEndOfString = EasyEx.selectWord();
+        System.out.println(chooseStartAndEndOfString);
+    }
+}
